a5f4a5bc38d6085e9a52544a91a429ef6a6e095b
Sergi <johndoe@example.com>
2020-10-21 17:36:33 +0200

    .

--- enc_dec.js
+++ enc_dec.js
@@ -27,3 +27,30 @@ function Decrypt(value)
   }
   return result;
 } 
+
+function hide_all_show_one(div_) {
+  var x = document.querySelectorAll(".dd"); //oculto tots els div amb class="dd"
+  var i;
+  for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+  } 
+  var x = document.getElementById(div_);
+  if (x.style.display === "none") 
+      x.style.display = "block";
+}
+
+function show_hide(div_) {
+  var x = document.getElementById(div_);
+  if (x.style.display === "none") { 
+      x.style.display = "block";
+      x.style.background = "silver";
+  }
+  else 
+      x.style.display = "none"; 
+}
+
+function opcions(n)
+{
+    for (i=0; i < n; i++)
+         document.write("<button onclick=\"hide_all_show_one('d"+i+"')\">Click Me</button>");
+}
